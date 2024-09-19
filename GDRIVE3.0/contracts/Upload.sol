953d7c59bbdaae59a16bb7f17101e8ad00dc911b
sherry_895 <shreyasuman9781@gmail.com>
2024-09-19 21:17:19 +0530

    Add files via upload

---
+++ GDRIVE3.0/contracts/Upload.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Upload {
+  
+  struct Access{
+     address user;//The address of the user who has access. 
+     bool access; //true or false
+  }
+  mapping(address=>string[]) value; //The address of the user who has access.
+  mapping(address=>mapping(address=>bool)) ownership;//indicating whether the second user has access to the first user’s files.
+  mapping(address=>Access[]) accessList;
+  mapping(address=>mapping(address=>bool)) previousData;
+
+  function add(address _user,string memory url) external {
+      value[_user].push(url);
+  }
+  function allow(address user) external {//def
+      ownership[msg.sender][user]=true; 
+      if(previousData[msg.sender][user]){
+         for(uint i=0;i<accessList[msg.sender].length;i++){
+             if(accessList[msg.sender][i].user==user){
+                  accessList[msg.sender][i].access=true; 
+             }
+         }
+      }else{
+          accessList[msg.sender].push(Access(user,true));  
+          previousData[msg.sender][user]=true;  
+      }
+    
+  }
+  function disallow(address user) public{
+      ownership[msg.sender][user]=false;
+      for(uint i=0;i<accessList[msg.sender].length;i++){
+          if(accessList[msg.sender][i].user==user){ 
+              accessList[msg.sender][i].access=false;  
+          }
+      }
+  }
+
+  function display(address _user) external view returns(string[] memory){
+      require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
+      return value[_user];
+  }
+
+  function shareAccess() public view returns(Access[] memory){
+      return accessList[msg.sender];
+  }
+}
